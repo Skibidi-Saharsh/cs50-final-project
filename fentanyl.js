b67e8df54f06affbe0c5852877701bf09517bfe1
Skibidi-Saharsh <coolsaharsh1980@gmail.com>
2025-05-26 11:54:15 -0400

    Create fentanyl.js

---
+++ fentanyl.js
@@ -0,0 +1,3 @@
+const image = document.createElement('img');
+image.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc5vS-BLEC9PEMuZN9sRfKd72lksTru1nHTA&s';
+document.body.appendChild(image);
